f5a0cfd666763cd1145f362e5346631d52b1d0b1
Zeeshan <zeeshanalivr46@gmail.com>
2025-12-18 20:03:05 +0530

    feat: initialized project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: {
+    compilationMode: "annotation",
+  },
+  allowedDevOrigins: ["localhost", "192.168.*.*"],
 };
 
 export default nextConfig;
