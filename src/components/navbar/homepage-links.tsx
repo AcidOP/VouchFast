7b1bb00377dbe27ef3c0d0f91cc8d6cf77b4a6d3
Zeeshan <zeeshanalivr46@gmail.com>
2025-12-18 22:25:36 +0530

    feat: navbar added

---
+++ src/components/navbar/homepage-links.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import links from '@/config/navlinks';
+
+import doubleArrow from '@/assets/double-arrow.svg';
+
+import Button from '@/components/ui/button';
+
+const HomepageLinks = async () => {
+  return (
+    <div className='flex items-center'>
+      <section className='absolute top-1/2 left-1/2 hidden -translate-x-1/2 -translate-y-1/2 lg:block'>
+        <ul className='flex space-x-4'>
+          {links.map(link => (
+            <li key={link.url}>
+              <Link href={link.url}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      {/* {!session && ( */}
+      <section className='hidden space-x-2 lg:flex'>
+        <Link href='/login'>
+          <Button variant='ghost'>Login</Button>
+        </Link>
+
+        <Link href='/auth'>
+          <Button className='gap-2'>
+            Get Started
+            <Image src={doubleArrow} height={20} width={20} alt='doodle' />
+          </Button>
+        </Link>
+      </section>
+      {/* )} */}
+    </div>
+  );
+};
+
+export default HomepageLinks;
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+// import AuthenticatedNavActions from './authenticated-navactions';
+import HomepageLinks from './homepage-links';
+
+import { cn } from '@/lib/utils';
+
+// import { grotesque } from '@/components/font/grotesque';
+
+const Navbar = () => {
+  return (
+    <nav className='bg-background sticky top-0 z-100'>
+      <div className='layout relative flex h-16 items-center justify-between text-lg'>
+        <h1 className='text-xl'>
+          <Link href={'/'} className='text-xl font-black lg:text-3xl'>
+            VouchFast
+          </Link>
+        </h1>
+
+        <HomepageLinks />
+        {/* <AuthenticatedNavActions /> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
