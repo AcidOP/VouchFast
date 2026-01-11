3377f6c8405799877566ef944c325bc966654ec5
Zeeshan <zeeshanalivr46@gmail.com>
2026-01-11 11:05:48 +0530

    fix: toast keeps re-rendering due to bad useEffect

--- src/hooks/use-toast.tsx
+++ src/hooks/use-toast.tsx
@@ -173,11 +173,9 @@ function useToast() {
     listeners.push(setState);
     return () => {
       const index = listeners.indexOf(setState);
-      if (index > -1) {
-        listeners.splice(index, 1);
-      }
+      if (index > -1) listeners.splice(index, 1);
     };
-  }, [state]);
+  }, []);
 
   return {
     ...state,
